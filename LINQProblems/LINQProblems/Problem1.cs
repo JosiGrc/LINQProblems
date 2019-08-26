018fccb09c9954fe703cc0977cf4108ccd5d4b0f
Josi <JosiGrc@users.noreply.github.com>
2019-08-26 10:19:43 -0500

    Started on the first LINQ problem

---
+++ LINQProblems/LINQProblems/Problem1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Problem1
+    {
+        //Varibales
+        List<string> words;
+        
+
+        //ctor
+        public Problem1()
+        {
+            List<string> words = new List<string>() {"the", "bike", "this", "it", "tenth", "mathematics" };
+        }
+
+        //Methods
+        //Using LINQ, write a function that returns all words that contain the substring “th” from a list
+        var newWords = new List<string>().Select();
+        var newWords = words.Select(w => w.)
+
+
+    }
+}
